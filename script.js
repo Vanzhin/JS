81fadf6eb6d2a1b2af7180756bc47900002a30ac
Nikolay <nikolay.vanzhin@yandex.ru>
2021-01-20 09:00:46 +0500

    create index.html and script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<!-- задание 1 -->
+let Tc = 20;
+let Tf = ((9 / 5) * Tc + 32);
+alert (Tf);
+<!-- задание 2 -->
+let name;
+let admin;
+name = "Василий";
+admin = name;
+alert (admin);
+<!-- задание 3 -->
+admin = 1000 + "108";
+alert (admin);
